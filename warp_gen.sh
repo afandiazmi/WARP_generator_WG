cde2397ab6e9dde8a05e3b6dff28787a8bf75803
Станислав Маркус <124920104+nae-stormrage@users.noreply.github.com>
2024-10-13 18:12:50 +0300

    Update warp_gen.sh

--- warp_gen.sh
+++ warp_gen.sh
@@ -30,25 +30,25 @@ for ((i=1; i<=num_configs; i++)); do
   peer_endpoint=$(echo "$peer_endpoint" | sed 's/\(.*\):[0-9]*/162.159.193.5/')
 
   conf=$(cat <<-EOM
-[Interface]
-PrivateKey = ${priv}
-S1 = 0
-S2 = 0
-Jc = 4
-Jmin = 40
-Jmax = 70
-H1 = 1
-H2 = 2
-H3 = 3
-H4 = 4
-Address = ${client_ipv4}, ${client_ipv6}
-DNS = 1.1.1.1, 2606:4700:4700::1111, 1.0.0.1, 2606:4700:4700::1001
+  [Interface]
+  PrivateKey = ${priv}
+  S1 = 0
+  S2 = 0
+  Jc = 4
+  Jmin = 40
+  Jmax = 70
+  H1 = 1
+  H2 = 2
+  H3 = 3
+  H4 = 4
+  Address = ${client_ipv4}, ${client_ipv6}
+  DNS = 1.1.1.1, 2606:4700:4700::1111, 1.0.0.1, 2606:4700:4700::1001
 
-[Peer]
-PublicKey = ${peer_pub}
-AllowedIPs = 0.0.0.0/1, 128.0.0.0/1, ::/1, 8000::/1
-Endpoint = ${peer_endpoint}:${port}
-EOM
+  [Peer]
+  PublicKey = ${peer_pub}
+  AllowedIPs = 0.0.0.0/1, 128.0.0.0/1, ::/1, 8000::/1
+  Endpoint = ${peer_endpoint}:${port}
+  EOM
   )
 
   clear
